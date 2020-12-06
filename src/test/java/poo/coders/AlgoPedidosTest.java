5676100bbe0612635f41c81d772e41b76646e420
Flor Patrone <patroneflorenciacarla@gmail.com>
2020-12-06 18:06:50 -0300

    test: testDeberiaPasar

---
+++ src/test/java/poo/coders/AlgoPedidosTest.java
@@ -0,0 +1,11 @@
+package poo.coders;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AlgoPedidosTest {
+    @Test
+    void testDeberiaPasar() {
+        Assertions.assertTrue(false);
+    }
+}
